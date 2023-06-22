aac370396984ba8101142de1601261a862bf66af
Arman Hossain <a.hossain@welldev.io>
2023-06-22 16:54:28 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_054631) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_105336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_054631) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "notice_title"
     t.index ["user_id"], name: "index_notices_on_user_id"
   end
 
