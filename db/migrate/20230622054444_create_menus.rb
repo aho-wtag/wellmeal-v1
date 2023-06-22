da79e387b36b56703a2db31c907466a6beccaa30
Arman Hossain <a.hossain@welldev.io>
2023-06-22 11:45:03 +0600

    Menu Model added

---
+++ db/migrate/20230622054444_create_menus.rb
@@ -0,0 +1,12 @@
+class CreateMenus < ActiveRecord::Migration[7.0]
+  def change
+    create_table :menus do |t|
+      t.integer :meal_type
+      t.date :meal_date
+      t.references :user, null: false, foreign_key: true
+      t.references :dish, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
