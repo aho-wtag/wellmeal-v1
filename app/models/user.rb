5954f0972577fab2dfe3099d773da37cd73b3f7f
Arman Hossain <a.hossain@welldev.io>
2023-06-22 15:01:35 +0600

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   has_one :review, through: :menus
   has_many :meal_attendances
   has_many :notices
+
+  enum role:  { user: 0, admin: 1 }
+  validates :email, :phone, :role, presence: true
+  validates :first_name, :last_name, presence: true, format:{with: /\A[A-Za-z]+\z/}
+  validates :email, :user_name, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, numericality: true, length: {minimum: 10, maximum: 15}
 end
