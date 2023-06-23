28f9d362a2148abc213d3a2a489f0f85aad0f044
Arman Hossain <42526536+arman-hosain@users.noreply.github.com>
2023-06-24 03:58:07 +0600

    test for Dish model

---
+++ spec/factories/dishes.rb
@@ -0,0 +1,7 @@
+require 'faker'
+FactoryBot.define do
+  factory :dish do
+    name { Faker::Food.dish }
+    ingredients { Faker::Food.ingredient }
+  end
+end
---
+++ spec/models/dish_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Dish, type: :model do
+  let(:dish) {FactoryBot.create(:dish)}
+
+  describe 'When creating a Dish' do
+    it 'should validate the presence of dish name' do
+       should validate_presence_of(:name)
+    end
+    it 'should validate the presence of dish name' do
+      should validate_presence_of(:ingredients)
+    end
+  end
+end
