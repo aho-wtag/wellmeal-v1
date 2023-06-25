85125c01aa0b6dc24a6f45866eb09d9ee130fc9e
Arman Hossain <a.hossain@welldev.io>
2023-06-25 14:53:59 +0600

    Created users_controller and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  before_action :find_user_by_id, only: %i[edit update show destroy]
+  def index
+    @users=User.all
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path(@user), notice: 'Successfully created account'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path(@user), status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = 'User was successfully deleted'
+    redirect_to users_path, status: :see_other
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :user_name, :phone,:role)
+  end
+
+  def find_user_by_id
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/destroy'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
