e45ed5ac1bcd0ca4f2a03e791080c63c67db124f
Arman Hossain <a.hossain@welldev.io>
2023-06-26 17:25:28 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class User < ApplicationRecord
   has_many :preferences
@@ -12,5 +11,5 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, format: { with: /\A[A-Za-z]+\z/ }
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone, phony_plausible: true
+  validates_plausible_phone :phone, presence: true
 end
