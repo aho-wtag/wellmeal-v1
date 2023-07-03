407383b294168a14f448dd08f35b87bf77cb4200
Arman Hossain <a.hossain@welldev.io>
2023-07-03 15:07:26 +0600

    Meal attendance model fixed

--- app/models/meal_attendance.rb
+++ app/models/meal_attendance.rb
@@ -2,7 +2,16 @@
 class MealAttendance < ApplicationRecord
   belongs_to :user
 
-  enum meal_type: { lunch: 0, snack: 1 }, presence:true
-  validates :meal_date, presence: true, date: { after_or_equal_to: -> { Date.current } }
+  enum meal_type: { lunch: 0, snack: 1 }
+  validates :meal_date, presence: true
+  validate :date_field_cannot_be_past
+
+  private
+
+  def date_field_cannot_be_past
+    if date_field.present? && date_field < Date.today
+      errors.add(:meal_date, "cannot be a past date")
+    end
+  end
 
 end
