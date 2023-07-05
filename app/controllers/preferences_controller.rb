0f628c52a09a42483ece901d60a821558482bb0f
Arman Hossain <a.hossain@welldev.io>
2023-07-05 09:48:28 +0600

    Preference controller and View

---
+++ app/controllers/preferences_controller.rb
@@ -0,0 +1,49 @@
+class PreferencesController < ApplicationController
+  before_action :find_menu_by_id, only: %i[edit update show destroy]
+  def index
+    @preferences = Preference.all
+  end
+
+  def new
+    @preference = Preference.new
+    @users=User.all
+  end
+
+  def create
+    @preference = Preference.new(preference_params)
+    if @preference.save
+      redirect_to preferences_path(@preference), notice: 'Successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @preference.update(preference_params)
+      redirect_to preferences_path(@preference), status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @preference.destroy
+    flash[:notice] = 'Dish was successfully deleted'
+    redirect_to preferences_path, status: :see_other
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:restricted_food, :user_id)
+  end
+
+  def find_menu_by_id
+    @preference = Preference.find(params[:id])
+    @users = User.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :menus
   resources :meal_attendances
   resources :notices
+  resources :preferences
   resources :reviews , only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
