d5f6709aad12fc89e317d4345f9ffc33d758cb91
Arman Hossain <a.hossain@welldev.io>
2023-07-06 11:33:45 +0600

    factory file fixed
    menu

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -4,13 +4,15 @@ FactoryBot.define do
   factory :menu do
     meal_date { Date.today }
     association :user
-    association :dish
     trait :lunch do
       meal_type { 0 }
     end
 
     trait :snack do
       meal_type { 1 }
+    end
+    after(:build) do |menu|
+      menu.dish << FactoryBot.build(:dish)
     end
   end
 end
