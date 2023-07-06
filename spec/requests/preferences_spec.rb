7da2220cc536086313d709221f0514eaec64da65
Arman Hossain <a.hossain@welldev.io>
2023-07-06 08:46:03 +0600

    preference controller test

---
+++ spec/requests/preferences_spec.rb
@@ -0,0 +1,42 @@
+
+require 'rails_helper'
+
+RSpec.describe 'Preferences', type: :request do
+  let!(:preference){FactoryBot.create(:preference)}
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/preferences'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/preferences/new'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get "/preferences/#{preference.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get "/preferences/#{preference.id}/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      expect do
+        delete "/preferences/#{preference.id}"
+      end.to change(Preference, :count).by(-1)
+    end
+  end
+end
