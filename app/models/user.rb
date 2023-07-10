fd486f544c87897444a2d69404a2218bb6ceaa44
Arman Hossain <a.hossain@welldev.io>
2023-07-10 16:05:46 +0600

    devise permit custom field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :preferences, dependent: :destroy
   has_many :menus, dependent: :destroy
   has_one :review, through: :menus, dependent: :destroy
   has_many :meal_attendances, dependent: :destroy
   has_many :notices, dependent: :destroy
 
-  enum role: { user: 0, admin: 1 }
+  enum role: { user: 0, admin: 1 , sup_admin: 2}
   validates :email, :phone, :role, presence: true
   validates :first_name, :last_name, presence: true, format: { with: /\A[A-Za-z]+\z/ }
   validates :email, uniqueness: { case_sensitive: false }
