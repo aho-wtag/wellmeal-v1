85f73cedcd598556a3e6a787f7aba0c2fc30cd84
Arman Hossain <a.hossain@welldev.io>
2023-07-11 12:46:54 +0600

    password field added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :user_name, :phone, :role)
+    params.require(:user).permit(:first_name, :last_name, :email, :user_name, :phone, :role, :password)
   end
 
   def find_user_by_id
