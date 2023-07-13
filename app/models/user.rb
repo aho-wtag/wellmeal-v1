03858ecf39fb88acffd83ce630b3b8283a817e35
Arman Hossain <a.hossain@welldev.io>
2023-07-13 10:05:57 +0600

    menu delete bug fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   has_many :preferences, dependent: :destroy
   has_many :menus, dependent: :destroy
-  has_one :review, through: :menus, dependent: :destroy
+  has_one :review, through: :menus
   has_many :meal_attendances, dependent: :destroy
   has_many :notices, dependent: :destroy
 
