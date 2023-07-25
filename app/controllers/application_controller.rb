9ce80b718e3223558c432516b31e5e4674d4cda3
Arman Hossain <a.hossain@welldev.io>
2023-07-25 10:57:36 +0600

    cancancan access denied defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do
+    flash.notice = 'Access Denied'
     redirect_to root_path
   end
   def index
     flash.notice = 'No page found at that address'
     redirect_to root_path
   end
-
   protected
 
   def configure_permitted_parameters
