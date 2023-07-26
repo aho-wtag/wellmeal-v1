cd84bcd637eb38a04ed10e8facf8f89cd3b60036
Arman Hossain <135516937+aho-wtag@users.noreply.github.com>
2023-07-26 10:43:06 +0600

    Update app/controllers/application_controller.rb
    Co-authored-by: Imtiaj Rafi <124483067+iud-wtag@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :phone,  :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :phone, :email, :password)}
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :phone, :email, :password, :current_password)}
   end
